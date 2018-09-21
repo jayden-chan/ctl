5873e3244c79e55b6f02c9a9b9a5b36a16c1227b
Jayden Chan <jaydencn7@gmail.com>
2018-09-21 10:20:58 -0700

    Add ESLint

--- handlers/api.js
+++ handlers/api.js
@@ -28,10 +28,10 @@ exports.register = async (args, callback) => {
       spinner.succeed('Account created. Login with the \'login\' command');
     })
     .catch(error => {
-      if(error.response.status === 400) {
+      if (error.response.status === 400) {
         spinner.fail(error.response.data);
       } else {
-        spinner.fail('Account creation attempt failed. Please try again later');
+        spinner.fail('Account creation failed. Please try again later (code '+error.response.status+')');
       }
     });
 
@@ -58,6 +58,17 @@ exports.login = async (args, callback) => {
     password: password.value
   })
     .then(response => {
+      let hasLogin = false;
+      fs.readFile(homedir + '/.netrc', function (err, data) {
+        if (err) throw err;
+        if (data.indexOf('machine ctl-server.herokuapp.com') >= 0) {
+          console.log(data)
+        }
+      });
+
+      if (hasLogin) {
+
+      }
       fs.appendFileSync(homedir + '/.netrc',
         'machine ctl-server.herokuapp.com\n'+
         '  login ' + email.value +
@@ -66,7 +77,7 @@ exports.login = async (args, callback) => {
       spinner.succeed('Successfully logged in');
     })
     .catch(error => {
-      if(error.response.status === 401) {
+      if (error.response.status === 401) {
         spinner.fail(error.response.data);
       } else {
         spinner.fail('Login attempt failed. Please try again later');
@@ -80,18 +91,18 @@ exports.logout = async(args, callback) => {
 
   // TODO: Finish implementing API endpoint
 
-//   const spinner = ora('Logging out...').start();
+  //   const spinner = ora('Logging out...').start();
 
-//   await axios.post('https://ctl-server.herokuapp.com/logout',)
-//     .then(response => {
-//       spinner.succeed('Successfully logged out');
-//     })
-//     .catch(error => {
-//       if(error.response.status === 401) {
-//         spinner.fail(error.response.data);
-//       } else {
-//         spinner.fail('Login attempt failed. Please try again later');
-//       }
-//     });
-//   callback();
+  //   await axios.post('https://ctl-server.herokuapp.com/logout',)
+  //     .then(response => {
+  //       spinner.succeed('Successfully logged out');
+  //     })
+  //     .catch(error => {
+  //       if(error.response.status === 401) {
+  //         spinner.fail(error.response.data);
+  //       } else {
+  //         spinner.fail('Login attempt failed. Please try again later');
+  //       }
+  //     });
+  //   callback();
 }
